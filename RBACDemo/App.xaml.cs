86ce19d4638564e40336655635bdbe4196219174
aimee.lee <995176547@qq.com>
2020-08-11 17:27:35 +0800

    修改

--- RBACDemo/App.xaml.cs
+++ RBACDemo/App.xaml.cs
@@ -1,10 +1,9 @@
 ﻿using RBACDemo.Assist;
-using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
 using System.Windows;
 
 namespace RBACDemo
@@ -17,12 +16,10 @@ namespace RBACDemo
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            string DBPath = "G:/Desktop/SqliteDemo/SqliteDemo/DemoDBFile.db";
-            //string DBPath= "pack://application:,,,/SqliteDemo/DemoDBFile.db";
-            string tableName = "AppUser";
-            List<string> fieldlist = new List<string>() {"ID","Name","Password"};
-            List<string> typelist = new List<string>() { FieldTypeAssist.INTEGERType, FieldTypeAssist.TEXTType, FieldTypeAssist.TEXTType };
-            DBFileAssist.CreatTable(DBPath,tableName,fieldlist,typelist);
-        }
+            SqliteAccessAssist.SetDefaultDB();
+           
+           
+        } 
+
     }
 }
--- RBACDemo/MainWindow.xaml.cs
+++ RBACDemo/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RBACDemo;
+using RBACDemo.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,19 +12,43 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace RBACDemo
+namespace SqliteDemo
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SignInWindow loginwin = new SignInWindow();
+        private UserPage userPage = new UserPage();
+        private GroupPage groupPage = new GroupPage();
+        private ActionPage actionPage = new ActionPage();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {         
+            loginwin.Show();
+        }
+
+        private void User_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = userPage;
+        }
+
+        private void Action_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = actionPage;
+        }
+
+        private void Group_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = groupPage;
         }
     }
 }
